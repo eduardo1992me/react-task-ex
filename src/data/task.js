725af16b694122a68ccf666ce907cd583b3a2136
Eduardo Medina <eduardo1992me@gmail.com>
2023-07-27 11:21:23 -0600

    first commit

---
+++ src/data/task.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "mi primer tarea",
+    descripcion: "mi  tarea uno",
+  },
+  {
+    id: 1,
+    title: "mi segunda tarea",
+    descripcion: "mi tarea dos",
+  },
+  {
+    id: 2,
+    title: "mi tercera tarea",
+    descripcion: "mi tarea tres",
+  },
+];
